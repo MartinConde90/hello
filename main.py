471a838a397613f58d5ecd2546cb4dfb6d84b29e
MartinCG <martincondegrande@hotmail.com>
2019-12-27 01:14:34 +0100

    uso de git

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print('Hello, world')
+
+#git init
+#git status
+#git commit -m
+#git log (da la clave que identifica nuestra version)
+#git remote add origin (y la direccion)
+#git remote -v, para comprobar que se ha creado bien
+#git push -u origin master, subir al repositorio, la "u" hace que sea el principal
